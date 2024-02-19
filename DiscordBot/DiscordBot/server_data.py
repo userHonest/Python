ec03d9b9c9a8a42e094a2c8580e1c5e854cc4806
user_honest <92337570+userHonest@users.noreply.github.com>
2024-02-20 00:14:09 +0100

    Delete DiscordBot/DiscordBot directory

--- DiscordBot/DiscordBot/server_data.py
+++
@@ -1,6 +0,0 @@
-BOT_TOKEN = 'paste your bot token here'
-server_ID = 'paste your server id here'
-channel_ID = 'paste your discord channel id here'
-
-
-# these variables are called in the bot.py script
